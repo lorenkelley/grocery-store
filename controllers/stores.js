4966bfdf5d400fcb3a388bcb5efcfd4ae26b7010
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-10-23 14:22:54 -0400

    working on stores.js in the controllers folder

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,16 +1,25 @@
 const Store = require('../models/Store')
 
 const storesController = {
-index:(req, res) => {
-    Store.find().populate('products')
-    // pull products inside a of store using .populate
-    .then(stores => {
-        res.render('stores/index',{
-        // res.render relates to handlebars (hbs)
-            stores: stores
-        })
-    })
-}
+    index: (req, res) => {
+        Store.find({}).populate('products')
+            .then((stores) => {
+                res.render('stores/index', {
+                    stores: stores
+
+                })
+            })
+    },
+    new: (req, res) => {
+        res.send("Hey this is new route on stores")
+    },
+    show: (req, res) => {
+        const storeId = req.params.storesId
+        Store.findById(storeId)
+            .then((store) => {
+                res.send(store)
+            })
+    }
 }
 
 module.exports = storesController
